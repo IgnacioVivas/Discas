c837aaea6e7f705e116184ffa2c8cd3d3c0e2be3
IgnacioVivas <ignacab@hotmail.com>
2025-04-23 16:33:32 -0300

    etiqueta label eliminada del footer

--- components/myComponents/MySuscripcion.tsx
+++ components/myComponents/MySuscripcion.tsx
@@ -2,7 +2,6 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
 import { inter, nunito, poppins } from '../ui/fonts';
 
